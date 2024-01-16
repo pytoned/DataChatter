852ea5f51ef3e71118807ac498d90063f730d641
pytoned <anders.lindsetmo@bayes.city.ac.uk>
2024-01-16 16:03:39 +0100

    deleted:    .env 	modified:   main.py

--- main.py
+++ main.py
@@ -4,20 +4,25 @@ import streamlit.components.v1 as components
 import pandas as pd
 import pygwalker as pyg
 from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
-from langchain_community.llms import OpenAI
-from langchain_community.chat_models import ChatOpenAI
+from langchain_openai import OpenAI
+from langchain_openai import ChatOpenAI
 from langchain.agents.agent_types import AgentType
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 from langchain.agents import load_tools
 import plotly.express as px
 
 
 def main():
     
-    load_dotenv()
-    
+    #load_dotenv()
+        
     st.set_page_config(page_title="Visualize and chat with your Excel/CSV files 📈", page_icon=":bar_chart:", layout="wide")
     st.header("Visualize and chat with your Excel/CSV files 🔎 📈")
+
+    with st.sidebar:
+        OPENAI_API_KEY = st.text_input("OpenAI API Key", type="password")
+        "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+
 
     user_file = st.file_uploader("Upload your Excel/CSV file ❣️", type=["csv", "xlsx"])
     
@@ -36,19 +41,37 @@ def main():
 
     if df is not None:
         
-        llm = OpenAI(temperature=0, max_tokens=1000)
-        agent = create_pandas_dataframe_agent(
-            ChatOpenAI(temperature=0, model="gpt-4"),
-            df,
-            verbose=True,
-            agent_type=AgentType.OPENAI_FUNCTIONS,
-            )
-           
-        user_question = st.text_input("Ask a question about your CSV file")
+        #llm = OpenAI(temperature=0, max_tokens=1000, OPENAI_API_KEY=OPENAI_API_KEY)
+        # agent = create_pandas_dataframe_agent(
+        #     ChatOpenAI(temperature=0, model="gpt-4"),
+        #     df,
+        #     verbose=True,
+        #     agent_type=AgentType.OPENAI_FUNCTIONS,
+        #     )
+        
+        def generate_response(input_text):
+            #llm = OpenAI(temperature=0, max_tokens=1000, OPENAI_API_KEY=OPENAI_API_KEY)
+            agent = create_pandas_dataframe_agent(
+                llm=ChatOpenAI(temperature=0, model="gpt-4"),
+                df=df,
+                verbose=True,
+                agent_type=AgentType.OPENAI_FUNCTIONS
+                )
+            st.info(agent.invoke(input_text))
+        
+        with st.form("my_form"):
+            user_question = st.text_area("Enter question:", "Ask a question about your Excel/CSV file?")
+            submitted = st.form_submit_button("Submit")
+            if not OPENAI_API_KEY:
+                st.info("Please add your OpenAI API key to continue.")
+            elif submitted:
+                generate_response(user_question)
+        
+        #user_question = st.text_input("Ask a question about your CSV file")
     
-        if user_question:
-            response = agent.run(user_question)
-            st.write(response)
+        # if user_question:
+        #     response = agent.invoke(user_question)
+        #     st.write(response)
         
         # Generate the HTML using PyGWalker
         pyg_html = pyg.walk(df, return_html=True)
